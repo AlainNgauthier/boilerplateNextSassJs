d825a65450cc9b12494d41d2cee21f3e68cb90ca
AlainNgauthier <ndamweyalaingauthier@ymail.com>
2021-12-13 01:33:24 -0300

    fixed mata tag names

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -5,12 +5,12 @@ function MyApp({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Programa de Estudantes Convênio de Graduação</title>
+        <title>Boilerplate for NextJS projects</title>
         <link rel="manifest" href="/manifest.json" />
-        <meta name="theme-color" content="#06092B" />
       </Head>
       <Component {...pageProps} />
     </>
-    )}
+  );
+}
 
-export default MyApp
+export default MyApp;
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -3,5 +3,5 @@ export default function Home() {
     <div>
       <span>Bla bla bla</span>
     </div>
-  )
+  );
 }
